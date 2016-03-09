4f8c1c682a1489eb724512fdf19fa02eb9b0d980
comp666 <comp622012016@mail.ru>
2016-03-09 14:59:14 +0300

    slojenie

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+	<title>slojenie</title>
+	<p>vvedite dan</p>
+</head>
+
+<body>
+
+<?php
+    $a=10;
+	$b=3;
+	$result=$a+$b;
+	echo "summa= ". $result;
+	
+ ?>
+	</body>
+</html>
